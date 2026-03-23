0bd8216968b80021f3aa6f3e7c7f38d9e6306e03
Vũ Long <lnvlong.dev@gmail.com>
2026-03-23 17:27:29 +0700

    feat: Add DFS traversal to Node class

--- scripts/models.py
+++ scripts/models.py
@@ -21,15 +21,19 @@ class Attribute:
 
 @dataclass
 class Relationship:
-    predicate: str
+    name: str
     target: Node
     qualifiers: Dict[str, List[QualifierUnit]] = field(default_factory=dict)
+
+    def __str__(self):
+        return f'Relationship{{Name: {self.name}}}'
 
 @dataclass
 class Node:
     id: str
     type: Literal['Concept', 'Entity']
     name: str
+    concept_node: Node | None
     attributes: List[Attribute]
     relationships: list[Relationship]
 
@@ -45,6 +49,25 @@ class Node:
             id=node['id'],
             type=type,
             name=node['name'],
+            concept_node=None,
             attributes=[],
             relationships=[]
         )
+    
+    def get_dfs_traversal(self)-> str:
+        return self._dfs_traverse('', set())
+    
+    def _dfs_traverse(self, current_str: str, node_id_set: set[str]) -> str:
+        if self.id in node_id_set:
+            return current_str
+        node_id_set.add(self.id)
+        if self.concept_node != None:
+            current_str += str(self) + '\n'
+            current_str += str(self) + ' is a ' + str(self.concept_node) + '\n'
+        for relationship in self.relationships:
+            current_str += f'Node{str(self)} Relationship{str(relationship)} Node{str(relationship.target)}\n'
+            relationship.target._dfs_traverse(current_str, node_id_set)
+        return current_str    
+
+    def __str__(self):
+        return f'Node{{Type: {self.type} | Name:{self.name}}}'
