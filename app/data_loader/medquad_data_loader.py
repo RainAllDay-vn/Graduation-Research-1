42a0966d14a974693f410a214746e9c003579908
lnvlong <lnvlong.dev@gmail.com>
2026-04-18 21:50:26 +0700

    feat: Create medquad_data_loader.py

---
+++ app/data_loader/medquad_data_loader.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+from typing import Optional
+from app.data_loader.contract import DataLoaderContract
+
+class MedquadDataLoader(DataLoaderContract):
+    def __init__(self, 
+        dataset_path: Optional[str] = None
+    ):
+        if dataset_path is None:
+            dataset_path = os.path.join('dataset', 'medquad', 'test.csv')
+
+        if not os.path.exists(dataset_path):
+            raise FileNotFoundError(f"Dataset path '{dataset_path}' is not a file.")
+
+        self.dataset_path = dataset_path
+
+    def load_dataset(self) -> list[(str, str)]:
+        data = pd.read_csv(self.dataset_path)
+
+        dataset = []
+        for _, row in data.iterrows():
+            question = row['question']
+            answer = row['answer']
+            dataset.append((question, answer))
+
+        return dataset
+
+    def load_dataset(self) -> list[(str, str)]:
+        raise NotImplementedError("Method not implemented")
+        
